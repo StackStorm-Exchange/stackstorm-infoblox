016bbf022f6d007a3de20368b1bd9333fbcb33c1
Hiroyasu OHYAMA <user.localhost2000@gmail.com>
2019-01-21 07:05:49 +0000

    Because of changing interface of get_object method, this corrected expected key name that is returned by its method from '_ref' to 'ref' at the action that calls that.

--- CHANGES.md
+++ CHANGES.md
@@ -1,5 +1,9 @@
 # Change Log
 
+## 0.3.0
+
+- Updated returned value format of get_action method from `_ref` to `ref`.
+
 ## 0.1.0
 
 - Initial Release of Infoblox integration
--- actions/restart_services.py
+++ actions/restart_services.py
@@ -9,9 +9,9 @@ class RestartAllMemberServicesAction(InfobloxBaseAction):
     def run(self):
         return [self.connection.call_func(**{
             'func_name': 'restartservices',
-            'ref': x['_ref'],
+            'ref': x['ref'],
             'payload': {
                 'restart_option': 'RESTART_IF_NEEDED',
                 'service_option': 'ALL'
             }
-        }) for x in self.connection.get_object('member') if x and '_ref' in x]
+        }) for x in self.connection.get_object('member') if x and 'ref' in x]
